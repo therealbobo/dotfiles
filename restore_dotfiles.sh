5cbd1a1e0772f1e7eb5756d61f46deac2d3c9c6e
therealbobo <robi.ayrton@gmail.com>
2019-07-11 18:48:05 +0200

    added rofi

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -64,6 +64,7 @@ ln -vfs $HOME/dotfiles/radare2rc $HOME/.radare2rc
 I3_DIR="$HOME/.config/i3"
 I3STATUS_DIR="$HOME/.config/i3status"
 POLYBAR_DIR="$HOME/.config/polybar"
+ROFI_DIR="$HOME/.config/rofi"
 TERMITE_DIR="$HOME/.config/termite"
 if ! test -d $I3_DIR ; then
 	mkdir -p $I3_DIR
@@ -74,9 +75,13 @@ fi
 if ! test -d $POLYBAR_DIR ; then
 	mkdir -p $POLYBAR_DIR
 fi
+if ! test -d $ROFI_DIR ; then
+	mkdir -p $ROFI_DIR
+fi
 ln -vfs $HOME/dotfiles/i3config $I3_DIR/config
 #ln -vfs $HOME/dotfiles/i3status $I3STATUS_DIR/config
 ln -vfs $HOME/dotfiles/polybarconfig $POLYBAR_DIR/config
+ln -vfs $HOME/dotfiles/roficonfig $ROFI_DIR/config
 ln -vfs $HOME/dotfiles/termiteconfig $TERMITE_DIR/config
 ln -vfs $HOME/dotfiles/termite.terminfo $HOME/termite.terminfo
 

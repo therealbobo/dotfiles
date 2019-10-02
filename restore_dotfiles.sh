466e4213eb9148fdff54e8fd6b4371a778592bf9
therealbobo <robi.ayrton@gmail.com>
2019-10-02 10:04:13 +0200

    refactor

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -2,12 +2,31 @@
 
 
 function install_packet {
+	RESULT=""
 	if egrep -qi "arch" "/etc/os-release"; then
-		sudo pacman -S $1
+		RESULT=$(sudo pacman -S $1)
 	elif egrep -qi "opensuse" "/etc/os-release"; then
-		sudo zypper install $1
+		RESULT=$(sudo zypper install $1)
 	elif egrep -qi "debian" "/etc/os-release"; then
-		sudo apt install $1
+		RESULT=$(sudo apt install $1)
+	fi
+
+
+	if $RESULT ; then
+		echo "[+] $1 installed";
+	else 
+		echo "[-] Cannot install $1. Plese install it manually. Then ^D"
+		bash
+	fi
+
+}
+
+
+check_pkg_installed (){
+	if test -f /usr/bin/$1 -o -f /bin/$1; then
+		echo "[-] no $1 installed"
+		echo "[-] installing $1"
+		install_packet $1
 	fi
 }
 
@@ -19,14 +38,10 @@ function install_vundle {
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 }
 
-
-#checks
-#zsh
-if ! test -f /usr/bin/zsh -o -f /bin/zsh; then
-	#install zsh
-	echo "no zsh installed"
-	install_packet "zsh"
-fi
+PACKAGES=("zsh" "vim" "tmux" "gdb" "i3" "polybar" "zathura" "rofi" "radare2")
+for PKG in $PACKAGES ; do
+	check_pkg_installed $PKG
+done
 
 #oh-my-zsh
 if ! test -d ~/.oh-my-zsh ; then
@@ -35,33 +50,21 @@ if ! test -d ~/.oh-my-zsh ; then
 	install_ohmyzsh
 fi
 
-#vim
-if ! test -f /usr/bin/vim -o -f /bin/vim; then
-	#install vim
-	echo "no vim installed"
-	install_packet "vim"
-fi
+#vundle 4 vim
 if ! test -d ~/.vim/bundle/Vundle.vim; then
 	#install Vundle
 	echo "no vundle installed"
 	install_vundle
 fi
 
-#tmux
-if ! test -f /usr/bin/tmux -o -f /bin/tmux; then
-	#install tmux
-	echo "no tmux installed"
-	install_packet "tmux"
-fi
-
 #create links
-ln -vfs $HOME/dotfiles/vimrc $HOME/.vimrc
+ln -vfs $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 mkdir -p $HOME/.vim/undodir
 mkdir -p $HOME/.vim/after/ftplugin
-ln -vfs $HOME/dotfiles/tex.vim $HOME/.vim/after/ftplugin
-ln -vfs $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -vfs $HOME/dotfiles/zshrc $HOME/.zshrc
-ln -vfs $HOME/dotfiles/radare2rc $HOME/.radare2rc
+ln -vfs $HOME/dotfiles/vim/tex.vim $HOME/.vim/after/ftplugin
+ln -vfs $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -vfs $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+ln -vfs $HOME/dotfiles/r2/radare2rc $HOME/.radare2rc
 
 
 I3_DIR="$HOME/.config/i3"
@@ -72,28 +75,22 @@ TERMITE_DIR="$HOME/.config/termite"
 SNIPPTES_DIR="$HOME/.vim/ultisnips"
 ZATHURA_DIR="$HOME/.config/zathura"
 
-if ! test -d $I3_DIR ; then
-	mkdir -p $I3_DIR
-fi
-if ! test -d $I3STATUS_DIR ; then
-	mkdir -p $I3STATUS_DIR
-fi
-if ! test -d $POLYBAR_DIR ; then
-	mkdir -p $POLYBAR_DIR
-fi
-if ! test -d $ROFI_DIR ; then
-	mkdir -p $ROFI_DIR
-fi
-if ! test -d $ZATHURA_DIR ; then
-	mkdir -p $ZATHURA_DIR
-fi
-ln -vfs $HOME/dotfiles/i3config $I3_DIR/config
-#ln -vfs $HOME/dotfiles/i3status $I3STATUS_DIR/config
-ln -vfs $HOME/dotfiles/polybarconfig $POLYBAR_DIR/config
-ln -vfs $HOME/dotfiles/polybar_launch.sh $POLYBAR_DIR/launch.sh
-ln -vfs $HOME/dotfiles/roficonfig $ROFI_DIR/config
-ln -vfs $HOME/dotfiles/termiteconfig $TERMITE_DIR/config
-ln -vfs $HOME/dotfiles/zathurarc $ZATHURA_DIR/zathurarc
+DIRS=($I3_DIR $I3STATUS_DIR $POLYBAR_DIR $ROFI_DIR $TERMITE_DIR $SNIPPTES_DIR $ZATHURA_DIR) 
+
+for DIR in $DIRS; do
+	if ! test -d $DIR ; then
+		mkdir -p $DIR
+	fi
+done
+
+ln -vfs $HOME/dotfiles/i3/i3config $I3_DIR/config
+#ln -vfs $HOME/dotfiles/i3/i3status $I3STATUS_DIR/config
+ln -vfs $HOME/dotfiles/polybar/polybarconfig $POLYBAR_DIR/config
+ln -vfs $HOME/dotfiles/polybar/polybar_launch.sh $POLYBAR_DIR/launch.sh
+ln -vfs $HOME/dotfiles/rofi/roficonfig $ROFI_DIR/config
+ln -vfs $HOME/dotfiles/termite/termiteconfig $TERMITE_DIR/config
+ln -vfs $HOME/dotfiles/gdb/gdbinit $HOME/.gdbinit
+ln -vfs $HOME/dotfiles/zathura/zathurarc $ZATHURA_DIR/zathurarc
 
 #restore plugins
 vim +PluginInstall +qa
@@ -101,4 +98,4 @@ mkdir -p "$HOME/.vim/spell"
 wget http://ftp.vim.org/pub/vim/runtime/spell/it.utf-8.spl -O "$HOME/.vim/spell"
 
 #restore snippets
-ln -vfs $HOME/dotfiles/ultisnips $SNIPPTES_DIR
+ln -vfs $HOME/dotfiles/vim/ultisnips $SNIPPTES_DIR

5f60e0cdf160fd18bfa4dc6970d45bc3a4e94593
therealbobo <robi.ayrton@gmail.com>
2020-02-26 23:08:34 +0100

    minor changes

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -12,6 +12,7 @@ function setup_vim (){
 	[ ! -d ~/.vim/bundle/Vundle.vim ] && install_vundle
 	vim +PluginInstall +qa
 	mkdir -p "$HOME"/.vim/spell
+	mkdir -p "$HOME"/.vim/after
 	wget http://ftp.vim.org/pub/vim/runtime/spell/it.utf-8.spl -O "$HOME/.vim/spell/it.utf-8.spl"
 	ln -fs "$HOME"/dotfiles/vim/ultisnips   "$HOME"/.vim
 	ln -fs "$HOME"/dotfiles/vim/tex.vim     "$HOME"/.vim/after/ftplugin

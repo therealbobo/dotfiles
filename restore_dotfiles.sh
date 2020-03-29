7b368758436059c9086a816977410cc7d7ad8ea1
therealbobo <robi.ayrton@gmail.com>
2020-03-29 15:47:57 +0200

    xdg initial refactor

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -3,4 +3,7 @@
 source "$HOME/dotfiles/scripts/pkg_man.sh"
 source "$HOME/dotfiles/scripts/utils.sh"
 
+mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/.local/share"
+
 "$HOME"/dotfiles/os_scripts/"$1".sh

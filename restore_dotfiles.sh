18c6f0cb0c965437ec68fbc9cd356cf509e4cff1
therealbobo <robi.ayrton@gmail.com>
2020-07-31 18:25:52 +0200

    refactor with stow

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -1,42 +1,6 @@
 #! /bin/bash
 
-#source "$HOME/dotfiles/scripts/pkg_man.sh"
-#source "$HOME/dotfiles/scripts/utils.sh"
-#
-#mkdir -p "$HOME/.local/bin"
-#mkdir -p "$HOME/.local/share"
-#
-#
-#while getopts "hlid:" opt; do
-#	case ${opt} in
-#		h ) # help message
-#			echo "Usage: $0 [-h|-l|-i -d <distro>]
-#		-h:	print this help message
-#		-l:	list distro scripts available
-#		-i:	install
-#		-d:	select distro"
-#			exit 0
-#			;;
-#		i ) # action
-#			INSTALL=install
-#			;;
-#		d ) # distro select
-#			DISTRO=${OPTARG}
-#			echo $DISTRO
-#			;;
-#		l ) # distro list
-#			SCRIPT=$(readlink -f $0)
-#			DISTRO_SCRIPTS_PATH=${SCRIPT%/*}/os_scripts
-#			echo 'Available distros:'
-#			ls $DISTRO_SCRIPTS_PATH | sed 's/.sh//'
-#			;;
-#		* )
-#			exit -1
-#			;;
-#	esac
-#done
-#
-#"$HOME"/dotfiles/os_scripts/"$DISTRO".sh "$INSTALL"
 
-command -v stow
+command -v stow 1>/dev/null || eval "echo '[!] Please install stow!'; exit"
 
+find * -maxdepth 0 -type d -exec stow {} \;

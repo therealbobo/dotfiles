2d4a9bfe9ea87ec4f3ced1aa523aaa0c3564db1e
therealbobo <robi.ayrton@gmail.com>
2020-08-01 12:06:44 +0200

    home links fixed

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -3,4 +3,4 @@
 
 command -v stow 1>/dev/null || eval "echo '[!] Please install stow!'; exit"
 
-find * -maxdepth 0 -type d -exec stow -v -R -t ~ {} \;
+find * -maxdepth 0 -type d -exec stow -v 1 -R -t ~ {} \;

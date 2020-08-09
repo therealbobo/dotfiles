6643cbca5932b15411a4acba27d0d954ca13d494
therealbobo <robi.ayrton@gmail.com>
2020-08-09 12:51:52 +0200

    vim restore

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -4,3 +4,8 @@
 command -v stow 1>/dev/null || eval "echo '[!] Please install stow!'; exit"
 
 find * -maxdepth 0 -type d -exec stow -v 1 -R -t ~ {} \;
+
+source util.sh
+install_ohmyzsh
+install_vundle
+setup_vim

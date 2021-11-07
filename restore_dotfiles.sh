d57ba8eda30ea926b40cda079e1d47dbc871d251
therealbobo <robi.ayrton@gmail.com>
2021-11-07 12:04:27 +0100

    posix compliance

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -1,23 +1,33 @@
-#! /bin/bash
+#! /bin/sh
 
-REDB='\033[01;31m'
-GREENB='\033[01;32m'
-BOLD='\033[1m'
-DEF='\033[00;39m'
+! command -v stow 1>/dev/null && echo '[!] Please install stow!' && exit 1
 
-source utils.sh
-
-! command -v stow 1>/dev/null && echo '[!] Please install stow!' && exit -1
-
-echo -e "$BOLD[-] Start stowing"
-echo -ne "$REDB"
-find * -maxdepth 0 -type d -exec stow -v 1 -R -t ~ {} \; 2>&1 | grep 'WARNING! unstowing' -A1
-echo -e "$GREENB[+] Done stowing$DEF"
+echo "[-] Start stowing"
+find ./* -maxdepth 0 -type d -exec stow -v 1 -R -t ~ {} \; 2>&1 | grep 'WARNING! unstowing' -A1
+echo "[+] Done stowing"
 
 
-echo -e "$BOLD[-] Setting up vim$DEF"
-setup_vim
-echo -ne "$GREENB"
+echo "[-] Setting up vim"
+
+if [ -z "$XDG_DATA_HOME" ]; then
+	echo '[!] XDG_DATA_HOME is not set. Please enter XDG_DATA_HOME: '
+	read -r XDG_DATA_HOME
+fi
+
+if [ -z "$XDG_CACHE_HOME" ]; then
+	echo '[!] XDG_CACHE_HOME is not set. Please enter XDG_CACHE_HOME: '
+	read -r XDG_CACHE_HOME
+fi
+
+mkdir -p "$XDG_DATA_HOME"/vim
+mkdir -p "$XDG_CACHE_HOME"/vim
+[ ! -f "$XDG_CACHE_HOME"/vim/viminfo ] && touch "$XDG_CACHE_HOME"/vim/viminfo
+
+# Install spell and snippests
+mkdir -p "$XDG_DATA_HOME"/vim/spell
+mkdir -p "$XDG_DATA_HOME"/vim/after
+mkdir -p "$XDG_DATA_HOME"/vim/pack
+wget http://ftp.vim.org/pub/vim/runtime/spell/it.utf-8.spl -O "$XDG_DATA_HOME/vim/spell/it.utf-8.spl"
+
 echo '[+] Done settinging up vim'
 echo '[+] All done!'
-echo -ne "$DEF"
